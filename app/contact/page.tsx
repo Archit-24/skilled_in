3974855681039abb29544fdebdb10d335a87cf44
Archit Aggarwal <155532480+Archit-24@users.noreply.github.com>
2024-11-16 17:19:36 +0000

    Almost done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      <h1>contact</h1>
-    </div>
+    <>
+    </>
   )
 }
 
--- components/team.tsx
+++ components/team.tsx
@@ -34,9 +34,16 @@ const people = [
 
 export function AnimatedTooltipPreview() {
   return (
-    
+    <>
+    <div className="flex justify-center max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
+        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
+          Team Members
+        </h2>
+      </div>
+
     <div className="flex flex-row items-center justify-center mb-10 w-full">
       <AnimatedTooltip items={people} />
     </div>
+    </>
   );
 }
