483ac3952d418906aff9b42d7b6ef2af43ad1ab4
Archit Aggarwal <155532480+Archit-24@users.noreply.github.com>
2024-11-16 10:32:00 +0000

    Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import { AnimatedTestimonialsDemo } from "@/components/Animated-testimonials";
+import { BackgroundLinesDemo } from "@/components/Background-lines";
 import { TypewriterEffectSmoothDemo } from "@/components/HeroSection";
+import { NavbarDemo } from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+    <NavbarDemo />
     <TypewriterEffectSmoothDemo />
+    <AnimatedTestimonialsDemo />
+    <BackgroundLinesDemo />
     </>
   );
 }
