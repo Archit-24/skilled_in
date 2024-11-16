42eb8717cc2f5e8b54b20b4544071765f2484fd4
Archit Aggarwal <155532480+Archit-24@users.noreply.github.com>
2024-11-16 03:03:00 +0000

    New

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { ThreeDCardDemo } from "@/components/3dcardcomponent";
+import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <ThreeDCardDemo />
+    <AnimatedTestimonials />
+    </>
+  );
+}
