5ad0007fa9fe8b5762e8b49d1f834acc53637a6f
Archit Aggarwal <155532480+Archit-24@users.noreply.github.com>
2024-11-16 15:42:00 +0000

    Navigation

---
+++ app/platform/page.tsx
@@ -0,0 +1,12 @@
+import { HeroParallaxDemo } from '@/components/Aboutparalex'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <HeroParallaxDemo/>
+    </div>
+  )
+}
+
+export default page
