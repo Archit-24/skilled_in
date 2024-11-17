24ffe8efb1fe75339352b6961af6894479e76b66
Archit Aggarwal <155532480+Archit-24@users.noreply.github.com>
2024-11-17 10:52:20 +0000

    Half done

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,22 @@
+
+import { AppleCardsCarouselDemo } from '@/components/cardcarosel'
+import { FloatingDockDemo } from '@/components/fltdock'
+import { GoogleGeminiEffectDemo } from '@/components/gemini'
+import { MultiStepLoaderDemo } from '@/components/multistep'
+import { WavyBackgroundDemo } from '@/components/wvbg'
 import React from 'react'
+import { useRef, useState } from "react"
 
 
 function page() {
   return (
     <>
-    <h1>Hello</h1>
+    <FloatingDockDemo/>
+    {/*<WavyBackgroundDemo/>}*/}
+    <AppleCardsCarouselDemo/>
+    <GoogleGeminiEffectDemo/>
+    <MultiStepLoaderDemo/>
+
     </>
   )
 }
