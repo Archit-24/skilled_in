33691d34f588418353b6225774e2c94b954a64af
Archit Aggarwal <155532480+Archit-24@users.noreply.github.com>
2024-11-17 05:15:11 +0000

    Before signin

--- app/platform/page.tsx
+++ app/platform/page.tsx
@@ -1,11 +1,13 @@
 import { HeroParallaxDemo } from '@/components/Aboutparalex'
 import { TimelineDemo } from '@/components/AboutTimeline'
+import { NavbarDemo } from '@/components/Navbar'
 import { AnimatedTooltipPreview } from '@/components/team'
 import React from 'react'
 
 function page() {
   return (
     <div>
+      <NavbarDemo/>
       <HeroParallaxDemo/>
       <TimelineDemo/>
       <AnimatedTooltipPreview/>
