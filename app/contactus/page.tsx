506da6635f8191121ea42b81a76a321685b5ccae
Archit Aggarwal <155532480+Archit-24@users.noreply.github.com>
2024-12-10 11:43:36 +0000

    Adding components

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -1,14 +1,28 @@
 import FaqSection from '@/components/animata/accordion/faq'
+import CardSpread from '@/components/animata/card/cardspread'
+import CommentReplyCard from '@/components/animata/card/commentreply'
+import FlipCard from '@/components/animata/card/flipcard'
+import FlowerMenu from '@/components/animata/list/flowermenu'
+import { GlobeDemo } from '@/components/contactglobe'
+import { SignupFormDemo } from '@/components/feedbackform'
 import { FloatingDockDemo } from '@/components/fltdock'
 import { FileUploadDemo } from '@/components/Upload'
+import { WorldMapDemo } from '@/components/worldmap'
 import React from 'react'
 
 function page() {
   return (
     <div>
       <FloatingDockDemo/>
+      <WorldMapDemo/>
+      <SignupFormDemo/>
+      {/* <GlobeDemo/> */}
       {/* <FileUploadDemo/>  */}
       {/* <FaqSection/> */}
+      {/* <CardSpread/> */}
+      {/* <CommentReplyCard/> */}
+      {/* <FlowerMenu/> */}
+      {/* <FlipCard/> */}
     </div>
 
   )
