330e46246831e75600699db955d0cb6d03eae25c
Archit Aggarwal <155532480+Archit-24@users.noreply.github.com>
2024-12-12 14:58:36 +0000

    Sign in commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-
+"use client";
 import { AppleCardsCarouselDemo } from '@/components/cardcarosel'
 import { FloatingDockDemo } from '@/components/fltdock'
 import { GoogleGeminiEffectDemo } from '@/components/gemini'
@@ -6,15 +6,22 @@ import { LampDemo } from '@/components/GlowingLamp'
 import { InfiniteMovingCardsDemo } from '@/components/infiniteCards'
 import { MultiStepLoaderDemo } from '@/components/multistep'
 import { WavyBackgroundDemo } from '@/components/wvbg'
+import { UserButton, useUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation';
 import React from 'react'
 import { useRef, useState } from "react"
 
 
 function page() {
+  const {user} = useUser();
+  if (!user){
+    redirect('/')
+  }
   return (
     <>
     <GoogleGeminiEffectDemo/>
     <FloatingDockDemo/>
+    <UserButton/>
     {/* <WavyBackgroundDemo/> */}
     {/* <AppleCardsCarouselDemo/> */}
     <InfiniteMovingCardsDemo/>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import { AnimatedTestimonialsDemo } from "@/components/Animated-testimonials";
 import { BackgroundLinesDemo } from "@/components/Background-lines";
 import { BackgroundBeamsWithCollisionDemo } from "@/components/BeamBackground";
 import { TypewriterEffectSmoothDemo } from "@/components/HeroSection";
 import { NavbarDemo } from "@/components/Navbar";
 import Image from "next/image";
+import {useUser} from '@clerk/nextjs'
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const {user} = useUser();
+  if (user){
+    redirect('/dashboard')
+  }
   return (
     <>
     <NavbarDemo />
