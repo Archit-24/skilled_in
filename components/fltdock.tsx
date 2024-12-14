4d6ab176b197798950c0d38e0d7e65c41cd520df
Archit24 <architagg2024@gmail.com>
2024-12-15 00:53:35 +0530

    Profile Page

--- components/fltdock.tsx
+++ components/fltdock.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FloatingDock } from "@/components/ui/floating-dock";
 import {
   IconBodyScan,
@@ -21,6 +21,29 @@ import { div } from "framer-motion/client";
 import { UserButton } from "@clerk/nextjs";
 
 export function FloatingDockDemo() {
+  const DotIcon = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        fill="currentColor"
+      >
+        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+      </svg>
+    );
+  };
+  const ProfileIcon = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+      >
+        <path d="M12 2a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm0 2a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm0 9c-5.33 0-8 2.667-8 5.333V20h16v-1.667C20 15.667 17.33 13 12 13zm-6 5.333c0-.938 2.272-3.333 6-3.333s6 2.395 6 3.333V18H6v-.667z" />
+      </svg>
+    );
+  };
+  
   const links = [
     {
       title: "Home",
@@ -77,16 +100,22 @@ export function FloatingDockDemo() {
   return (
     // <div className="fixed top-0">
     <div className="fixed top-[-190px] flex items-center h-[35rem] w-full">
-      
       <FloatingDock
         mobileClassName="translate-y-20" // only for demo, remove for production
         items={links}
-        />
-      
-      <div>
-        <UserButton showName/>
-      </div>
+      />
+      <UserButton>
+        <UserButton.MenuItems>
+          <UserButton.Link
+            label="SkilledIn Profile"
+            labelIcon={<ProfileIcon />}
+            href="/skilledin-profile"
+          />
+          <UserButton.Action label="manageAccount" />
+        </UserButton.MenuItems>
+      </UserButton>
     </div>
+
     // </div>
   );
 }
